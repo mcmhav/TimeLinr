59dcda775b82cd03b38a24d3b859ba855207308c
mcmhav <mcmhav@gmail.com>
2016-02-29 00:59:39 +0100

    chagned to use new todo component

--- src/components/todos/todos.jsx
+++ src/components/todos/todos.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Todo from '../todo/todo';
 
 class Todos extends React.Component {
   render() {
     return (
       <div className="todos">
-        <div></div>
+        <Todo />
       </div>
     );
   }
